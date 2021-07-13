695922f000cec49c0d3026108f3f0685c74a3be3
Jovane Mafort <jmafort@id.uff.br>
2021-07-13 13:00:30 -0300

    some small changes to the code

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,33 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
-import { AlurakutStyles } from '../src/lib/AlurakutCommons'
+import { AlurakutStyles } from '../src/lib/AlurakutCommons';
 
 const GlobalStyle = createGlobalStyle`
-  *{
+  /* Reset CSS (Necolas Reset CSS <3) */
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-
   body {
-    background-color: #d9e6f6;
     font-family: sans-serif;
+    background-color: #D9E6F6;
   }
-
-  #__next{
+  #__next {
     display: flex;
     min-height: 100vh;
     flex-direction: column;
   }
-
-  img{
+  img {
     max-width: 100%;
     height: auto;
     display: block;
   }
-
   ${AlurakutStyles}
 `
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: 'red',
   },
 }
 
